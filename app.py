b8761d280e20e920175bd5abed785c1cd661064a
Maik <maik@mac.speedport.ip>
2026-02-10 17:40:57 +0100

    Prepare for Railway

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("FLASK_PORT", "3000"))
-    debug = os.getenv("FLASK_DEBUG", "1") == "1"
-    app.run(debug=debug, port=port)
+    port = int(os.getenv("PORT", os.getenv("FLASK_PORT", "3000")))
+    debug = os.getenv("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", debug=debug, port=port)
